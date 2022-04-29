f1b1f870e8bd5232754eb299eac4fd6c06dac7a6
OmarRaihan <omarraihan91@gmail.com>
2022-04-29 23:24:44 +0600

    header added

---
+++ src/Pages/Login/Social/Social.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Social = () => {
+    return (
+        <div>
+            <h2>This is Social Login</h2>
+        </div>
+    );
+};
+
+export default Social;
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar className="shadow py-3" bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand className="fw-bold fs-4" as={Link} to="/home">
+            Bike Town
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto fw-bold">
+              <Nav.Link as={Link} to="/home">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/blogs">
+                Blogs
+              </Nav.Link>
+              <Nav.Link as={Link} to="/about">
+                About
+              </Nav.Link>
+              <Nav.Link as={Link} to="/login">
+                Login
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
