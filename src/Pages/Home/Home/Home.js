54a2fa9fee4c792b5f23e375d396834e1622bed6
OmarRaihan <omarraihan91@gmail.com>
2022-04-29 23:59:39 +0600

    banner added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,50 +1,22 @@
-import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import banner from "../../../images/images/Banner-1.jpg";
 
 const Home = () => {
-    return (
-        <div>
-            <h2>This is home</h2>
-            {/* Carousel */}
-            <Carousel fade>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=First slide&bg=373940"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Second slide&bg=282c34"
-      alt="Second slide"
-    />
-
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="holder.js/800x400?text=Third slide&bg=20232a"
-      alt="Third slide"
-    />
-
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-        </div>
-    );
+  return (
+    <div>
+      <Carousel fade>
+        <Carousel.Item>
+          <img className="d-block w-100" src={banner} alt="banner" />
+          <Carousel.Caption>
+            <h3>Here's the Great Deal</h3>
+            <p>Grab your hidden beast</p>
+            <button style={{backgroundColor: 'orangeRed'}} className="btn rounded-pill text-white w-25">Book Now</button>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
 };
 
 export default Home;
