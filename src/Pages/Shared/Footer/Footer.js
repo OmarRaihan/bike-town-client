c18e01bc7a0a1ce73a53f57be78994a57d22c132
OmarRaihan <omarraihan91@gmail.com>
2022-04-30 21:30:27 +0600

    footer re design

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,76 +1,77 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "./Footer.css";
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
+  const today = new Date();
+  const year = today.getFullYear();
   return (
-      <div class="footer-clean">
-        <footer>
-          <div class="container">
-            <div class="row justify-content-center">
-              <div class="col-sm-4 col-md-3 item">
-                <h3>Services</h3>
-                <ul>
-                  <li>
-                    <a href="#">Bikes</a>
-                  </li>
-                  <li>
-                    <a href="#">Accessories</a>
-                  </li>
-                  <li>
-                    <a href="#">Equipments</a>
-                  </li>
-                </ul>
-              </div>
-              <div class="col-sm-4 col-md-3 item">
-                <h3>About</h3>
-                <ul>
-                  <li>
-                    <a href="#">Company</a>
-                  </li>
-                  <li>
-                    <a href="#">Team</a>
-                  </li>
-                  <li>
-                    <a href="#">Legacy</a>
-                  </li>
-                </ul>
-              </div>
-              <div class="col-sm-4 col-md-3 item">
-                <h3>Careers</h3>
-                <ul>
-                  <li>
-                    <a href="#">Job openings</a>
-                  </li>
-                  <li>
-                    <a href="#">Employee success</a>
-                  </li>
-                  <li>
-                    <a href="#">Benefits</a>
-                  </li>
-                </ul>
-              </div>
-              <div class="col-lg-3 item social">
-                {/* <a href="#">
-                  <i class="icon ion-social-facebook"></i>
-                  <FontAwesomeIcon icon={fa} />
-                </a>
-                <a href="#">
-                  <i class="icon ion-social-twitter"></i>
-                </a>
-                <a href="#">
-                  <i class="icon ion-social-snapchat"></i>
-                </a>
-                <a href="#">
-                  <i class="icon ion-social-instagram"></i>
-                </a> */}
-                <p class="copyright">All rights reserved &copy; 2022</p>
-              </div>
+    <div class="footer-clean">
+      <footer>
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-sm-4 col-md-3 item">
+              <h3>Services</h3>
+              <ul>
+                <li>
+                  <a href="#">Bikes</a>
+                </li>
+                <li>
+                  <a href="#">Accessories</a>
+                </li>
+                <li>
+                  <a href="#">Equipments</a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-sm-4 col-md-3 item">
+              <h3>About</h3>
+              <ul>
+                <li>
+                  <a href="#">Company</a>
+                </li>
+                <li>
+                  <a href="#">Team</a>
+                </li>
+                <li>
+                  <a href="#">Legacy</a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-sm-4 col-md-3 item">
+              <h3>Careers</h3>
+              <ul>
+                <li>
+                  <a href="#">Job openings</a>
+                </li>
+                <li>
+                  <a href="#">Employee success</a>
+                </li>
+                <li>
+                  <a href="#">Benefits</a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-lg-3 item social">
+              <a href="#">
+                <i class="fa-brands fa-facebook-f"></i>
+              </a>
+              <a to="#">
+                <i class="fa-brands fa-twitter"></i>
+              </a>
+              <a href="#">
+                <i class="fa-brands fa-linkedin-in"></i>
+              </a>
+              <a href="#">
+                <i class="fa-brands fa-instagram"></i>
+              </a>
+              <p class="copyright">All rights reserved &copy; {year}</p>
             </div>
           </div>
-        </footer>
-      </div>
+        </div>
+      </footer>
+    </div>
   );
 };
 
