17d8c683674696049af49bfb5be93754488b8525
OmarRaihan <omarraihan91@gmail.com>
2022-05-02 12:02:03 +0600

    register page setup

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,77 +1,77 @@
-// import React, { useState } from "react";
-// import { Button, Form } from "react-bootstrap";
-// import { useCreateUserWithEmailAndPassword, useUpdateProfile } from "react-firebase-hooks/auth";
-// import { Link, useNavigate } from "react-router-dom";
-// import auth from "../../../firebase.init";
-// import Loading from "../../Shared/Loading/Loading";
-// import Social from "../Social/Social";
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
+import Loading from "../../Shared/Loading/Loading";
+import Social from "../Social/Social";
 
-// const Register = () => {
-//   const [agree, setAgree] = useState(false);
-//   const [createUserWithEmailAndPassword, loading] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+const Register = () => {
+  const [agree, setAgree] = useState(false);
+  const [createUserWithEmailAndPassword, loading] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
 
-//   const [updateProfile, updating] = useUpdateProfile(auth);
+  const [updateProfile, updating] = useUpdateProfile(auth);
 
-//   const navigate = useNavigate();
+  const navigate = useNavigate();
 
-//   const navigateLogin = () => {
-//     navigate("/login");
-//   };
+  const navigateLogin = () => {
+    navigate("/login");
+  };
 
-//   if (loading || updating) {
-//     return <Loading></Loading>;
-//   }
+  if (loading || updating) {
+    return <Loading></Loading>;
+  }
 
-//   const handleRegister = async (event) => {
-//     event.preventDefault();
-//     const name = event.target.name.value;
-//     const email = event.target.email.value;
-//     const password = event.target.password.value;
+  const handleRegister = async (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
 
-//     await createUserWithEmailAndPassword(email, password);
-//     await updateProfile({ displayName: name });
-//     console.log("Updated profile");
-//     navigate("/home");
-//   };
-//   return (
-//     <div className="container w-50 mx-auto my-5">
-//       <h3 className="text-center mb-3">Please SignUp</h3>
-//       <Form onSubmit={handleRegister}>
-//         <Form.Group className="mb-3" controlId="formBasicEmail">
-//           <Form.Control type="text" name="name" placeholder="Name" required />
-//         </Form.Group>
+    await createUserWithEmailAndPassword(email, password);
+    await updateProfile({ displayName: name });
+    console.log("Updated profile");
+    navigate("/home");
+  };
+  return (
+    <div className="container w-50 mx-auto my-5">
+      <h3 className="text-center mb-3">Please SignUp</h3>
+      <Form onSubmit={handleRegister}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control type="text" name="name" placeholder="Name" required />
+        </Form.Group>
 
-//         <Form.Group className="mb-3" controlId="formBasicEmail">
-//           <Form.Control type="email" name="email" placeholder="Email" required />
-//         </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control type="email" name="email" placeholder="Email" required />
+        </Form.Group>
 
-//         <Form.Group className="mb-3" controlId="formBasicPassword">
-//           <Form.Control type="password" name="password" placeholder="Password" required />
-//         </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Control type="password" name="password" placeholder="Password" required />
+        </Form.Group>
 
-//         <Form.Group className="mb-2" controlId="formBasicCheckbox">
-//           <Form.Check
-//             onClick={() => setAgree(!agree)}
-//             type="checkbox"
-//             name="terms"
-//             className={agree ? "text-primary" : "text-danger"}
-//             label="Accept Terms and Conditions"
-//           />
-//         </Form.Group>
+        <Form.Group className="mb-2" controlId="formBasicCheckbox">
+          <Form.Check
+            onClick={() => setAgree(!agree)}
+            type="checkbox"
+            name="terms"
+            className={agree ? "text-primary" : "text-danger"}
+            label="Accept Terms and Conditions"
+          />
+        </Form.Group>
 
-//         <Button style={{backgroundColor: "orangeRed"}} disabled={!agree} className="w-100 border-0" type="submit" value="Register">
-//           Sign Up
-//         </Button>
-//       </Form>
-//       <p className="mt-2">
-//         Already have an account?
-//         <Link to="/login" className="text-danger text-decoration-none pe-auto ms-1" onClick={navigateLogin}>
-//           Please Login
-//         </Link>
-//       </p>
-//       <Social></Social>
-//     </div>
-//   );
-// };
+        <Button style={{ backgroundColor: "orangeRed" }} disabled={!agree} className="w-100 border-0" type="submit" value="Register">
+          Sign Up
+        </Button>
+      </Form>
+      <p className="mt-2">
+        Already have an account?
+        <Link to="/login" className="text-danger text-decoration-none pe-auto ms-1" onClick={navigateLogin}>
+          Please Login
+        </Link>
+      </p>
+      <Social></Social>
+    </div>
+  );
+};
 
-// export default Register;
+export default Register;
