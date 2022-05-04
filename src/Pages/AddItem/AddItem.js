5e1ad5efbb992c71ebbd414fe1e2f2733ef98f2e
OmarRaihan <omarraihan91@gmail.com>
2022-05-04 11:07:36 +0600

    add item completed

--- src/Pages/AddItem/AddItem.js
+++ src/Pages/AddItem/AddItem.js
@@ -3,23 +3,41 @@ import { useForm } from "react-hook-form";
 
 const AddItem = () => {
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    console.log(data);
+
+    const url = `http://localhost:7000/bike`;
+    fetch(url, {
+    method: 'POST', 
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+  .then(res=> res.json())
+  .then(result => {
+    console.log('success', result);
+  })
+  };
 
   return (
-    <div className="container w-50 mx-auto my-5">
+    <div style={{backgroundColor: '#a8dadc', width: '35rem', padding: '1.3rem'}} className="container w-50 mx-auto my-5 rounded-2">
+
       <h4 className="text-center mb-3">Add New Product</h4>
       <form className="d-flex flex-column" onSubmit={handleSubmit(onSubmit)}>
-        <input className="mb-2 rounded-1 p-2" placeholder="Name" {...register("name", { required: true })} />
+        <input className="mb-2 border-0 rounded-1 p-2" placeholder="Name" type="text" {...register("name", { required: true })} />
 
-        <textarea className="mb-2 rounded-1 p-2" placeholder="Description" {...register("description")} />
+        <textarea className="mb-2 border-0 rounded-1 p-2" placeholder="Description" type="text" {...register("description")} />
 
-        <input className="mb-2 rounded-1 p-2" placeholder="Price" type="number" {...register("price")} />
+        <input className="mb-2 border-0 rounded-1 p-2" placeholder="Price" type="number" {...register("price")} />
 
-        <input className="mb-2 rounded-1 p-2" placeholder="Quantity" type="number" {...register("quantity")} />
+        <input className="mb-2 border-0 rounded-1 p-2" placeholder="Quantity" type="number" {...register("quantity")} />
 
-        <input className="mb-2 rounded-1 p-2" placeholder="Supplier" {...register("supplier", { required: true })} />
+        <input className="mb-2 border-0 rounded-1 p-2" placeholder="Supplier" type="text" {...register("supplier", { required: true })} />
 
-        <input className="border-0 rounded-1 p-3 text-white" style={{ backgroundColor: "orangeRed" }} type="submit" value="Add" />
+        <input className="mb-2 border-0 rounded-1 p-2" placeholder="Photo URL" type="text" {...register("img", { required: true })} />
+
+        <input className="border-0 rounded-1 p-3 text-white fs-5" style={{ backgroundColor: "orangeRed" }} type="submit" value="Add" />
       </form>
     </div>
   );
