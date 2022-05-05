421a067f9804b2886b581bcc81e2e32b6ff96543
OmarRaihan <omarraihan91@gmail.com>
2022-05-05 16:12:13 +0600

    handle home button

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import './NotFound.css'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./NotFound.css";
 
 const NotFound = () => {
-    return (
-        <div style={{height: '50vh'}} className="error-page text-center my-5">
-            <h1 className='headline'>That Page Doesn't Exist!!</h1>
-            <p>Oops!!! Sorry, the page you were looking for could not be found</p>
+  const navigate = useNavigate();
 
-            <div className='my-2'>
-                <h1 className='error'>404</h1>
-            </div>
-        </div>
-    );
+  const handleHome = () =>{
+      navigate('/home')
+  }
+  return (
+    <div style={{ height: "50vh" }} className="error-page text-center my-5">
+      <h1 className="headline">That Page Doesn't Exist!!</h1>
+      <p>Oops!!! Sorry, the page you are looking for cannot be found</p>
+
+      <div className="mt-2 mb-5">
+        <h1 className="error">404</h1>
+      </div>
+      <div>
+        <button style={{ backgroundColor: "orangeRed" }} className="btn text-white text-decoration-none border-0 pe-auto px-5" onClick={handleHome}>
+          Go to Home
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
