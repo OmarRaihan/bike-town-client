a303ff2ae767a47faa513a0811d39f3dc92b5a9e
OmarRaihan <omarraihan91@gmail.com>
2022-05-05 15:57:09 +0600

    design 404 page

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div className="text-center my-5">
-            <h1>404</h1>
-            <h4>Oops!Page Not Found</h4>
+        <div style={{height: '50vh'}} className="error-page text-center my-5">
+            <h1 className='headline'>That Page Doesn't Exist!!</h1>
+            <p>Oops!!! Sorry, the page you were looking for could not be found</p>
+
+            <div className='my-2'>
+                <h1 className='error'>404</h1>
+            </div>
         </div>
     );
 };
