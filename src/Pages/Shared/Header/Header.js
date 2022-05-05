5cbc686c14a9c130255a5afd07e21f82d8d43365
OmarRaihan <omarraihan91@gmail.com>
2022-05-05 15:25:56 +0600

    login to manage

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -26,9 +26,12 @@ const Header = () => {
               <Nav.Link as={Link} to="/home">
                 Home
               </Nav.Link>
-              <Nav.Link as={Link} to="/manage">
+              {
+                user ? 
+                (<Nav.Link as={Link} to="/manage">
                 Manage
-              </Nav.Link>
+              </Nav.Link>): ''
+              }
 
               <NavDropdown title="Services" id="basic-nav-dropdown">
                 <NavDropdown.Item as={Link} to="/bikes">
