e693103d178528eee78e9d0b36e40c9031486173
OmarRaihan <omarraihan91@gmail.com>
2022-05-05 16:41:07 +0600

    404 page responsived

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -9,9 +9,9 @@ const NotFound = () => {
       navigate('/home')
   }
   return (
-    <div style={{ height: "50vh" }} className="error-page text-center my-5">
+    <div style={{ height: "100vh" }} className="error-page text-center my-5">
       <h1 className="headline">That Page Doesn't Exist!!</h1>
-      <p>Oops!!! Sorry, the page you are looking for cannot be found</p>
+      <p>Sorry, the page you are looking for cannot be found.</p>
 
       <div className="mt-2 mb-5">
         <h1 className="error">404</h1>
